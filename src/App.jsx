07b33e2c7f6ab26e9801c50d7e9c62392c069abb
devcaianno <caianno.dev@gmail.com>
2024-03-16 09:44:25 -0300

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,22 @@
+import Category from "./Components/Category"
+import Food from "./Components/Food"
+import HeadlinesCards from "./Components/HeadlinesCards"
+import Hero from "./Components/Hero"
+import Navbar from "./Components/Navbar"
+
+
+function App() {
+  
+
+  return (
+    <div>
+      <Navbar />
+      <Hero/>
+      <HeadlinesCards/>
+      <Food/>
+      <Category/>
+    </div>
+  )
+}
+
+export default App
---
+++ src/Components/Hero.jsx
@@ -0,0 +1,22 @@
+import React from 'react'
+import fotoB1 from './../assets/fotoB1.jpg'
+
+const Hero = () => {
+  return (
+    <div className='max-w-[1640px] mx-auto p-4'>
+        <div className='max-h-[500px] relative'>
+            {/* overlay */}
+            <div className='absolute w-full h-full text-gray-200 max-h-[500px] bg-black/40 flex flex-col justify-center'>
+                <h1 className='px-4 text-4xl sm:text-5xl md:tx-6xl lg:text-7xl font-bold'>The <span className='text-orange-500'>Best</span></h1>
+                <h1 className='px-4 text-4xl sm:text-5xl md:tx-6xl lg:text-7xl font-bold' ><span className='text-orange-500'>Foods</span> <span>Delivered</span></h1>
+            </div>
+            <img 
+                src={fotoB1} 
+                className='w-full max-h-[500px] object-cover'
+                alt="" />
+        </div>
+    </div>
+  )
+}
+
+export default Hero;
